55794bb1d83e7eaa8c6e37ac28fcb43ca256641d
Necrocraft <mhdzeefan@gmail.com>
2020-02-26 22:13:49 +0530

    Updated the logic with minor fixes for spaces

--- app.js
+++ app.js
@@ -7,32 +7,52 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/api/timestamp", (req, res) => {
+    let date = new Date();
+    res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+    })
+})
+
 app.get("/api/timestamp/:date_string", (req, res) => {
+    let unix, utc;
     let value = req.params.date_string;
-    console.log(typeof value);
-    if (isNaN(value)) {
-        let date = new Date(value);
-        if (date != "Invalid Date") {
-            res.json({
-                unix: date.getTime(),
-                utc: date.toUTCString()
-            });
-        } else {
-            res.json({
-                error: "Invalid Date"
-            })
+    if(isNaN(value)) {
+        let date = new Date(value.trim());
+        if(date != "Invalid Date") {
+        unix = date.getTime();
+        utc = date.toUTCString();
+        res.json({
+            unix: unix,
+            utc: utc
+        })
         }
-    } else {
-        console.log(value + " Value");
-        let date = new Date(parseInt(value));
-        if (date != "Invalid Date") {
+        else {
+            res.json({error: "Invalid Date"});
+        }
+    }
+    else {
+        if(parseInt(value) != NaN) {
+            let date = new Date(parseInt(value));
+            if(date != "Invalid Date") {
+            unix = date.getTime();
+            utc = date.toUTCString();
             res.json({
-                unix: value,
-                utc: date.toUTCString()
+                unix: unix,
+                utc: utc
             })
-        } else {
+            }
+            else {
+                res.json({error: "Invalid Date"});
+            }
+        }
+        else {
+            unix = 0;
+            utc = new Date(0).toUTCString();
             res.json({
-                error: "Invalid Date"
+                unix: unix,
+                utc: utc
             })
         }
     }
